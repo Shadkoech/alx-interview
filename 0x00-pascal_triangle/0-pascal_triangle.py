b603be3e26a01f27b70faab997d44afbed9b9def
Shadkoech <shadrackipruto@gmail.com>
2024-03-07 05:39:22 +0300

    Initial commit; Readme file

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module that generates Pascal's triangle
+"""
+
+
+def pascal_triangle(n):
+    """ A function that creates a Pascal's triable of dimension n
+    Args:
+        n represent the size of the triangle
+    Returns:
+        List of integers representing the Pascal's triangle
+        Returns an empty list if n <= 0
+    """
+
+
