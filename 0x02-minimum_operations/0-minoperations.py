eb22ff47a6f8ad9b8bb3386af57a3589ec595943
Shadkoech <shadrackipruto@gmail.com>
2024-03-21 22:41:24 +0300

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -13,19 +13,12 @@ def minOperations(n):
     Returns:
         min number of operations needed or 0 if n is impossible to achieve"""
 
+    min_ops = 0
     if n <= 1:
         return 0
 
-    # Initializing variables
-    min_ops = [0] * (n + 1)
-
     for i in range(2, n + 1):
-        min_ops[i] = i  # Initialize with worst case
-
-        # Check all possible lengths of substrings
-        for j in range(2, i):
-            if i % j == 0:
-                # If j divides i, we repeat a substring to form i characters
-                min_ops[i] = min(min_ops[i], min_ops[j] + (i // j))
-
-    return min_ops[n]
+        while n % i == 0:
+            n = n / i
+            min_ops += i
+    return min_ops
