098cbdb445629cdb5db5598a675d66882c269477
Shadkoech <shadrackipruto@gmail.com>
2024-03-21 22:45:32 +0300

    Pycodestyle fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -13,12 +13,18 @@ def minOperations(n):
     Returns:
         min number of operations needed or 0 if n is impossible to achieve"""
 
-    min_ops = 0
+    min_ops = 0  # Initialize the total operations needed
+
     if n <= 1:
         return 0
 
+    # Iterate through numbers from 2 to n
     for i in range(2, n + 1):
+        # While n is divisible by i,
+        # keep dividing n by i and increment operations by i
         while n % i == 0:
-            n = n / i
+            n = n / i  # Divide n by i
+
+            # Increment operations by i, as i rep number of times 'i' is pasted
             min_ops += i
     return min_ops
